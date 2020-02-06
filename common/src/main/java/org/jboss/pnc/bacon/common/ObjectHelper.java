b689f165b16b0eae85a956a1da454efe9507a661
Dustin Kut Moy Cheung <dcheung@redhat.com>
2020-02-06 16:29:46 -0500

    [NCL-5452] Allow user to specify verbosity
    This commit add the '-v' or '--verbose' flag to `AbstractCommand` to allow the
    user to specify the desired verbosity of the output. The verbose flag
    should therefore be available to all the commands.
    
    All the log output is printed to stderr, as before. This is so that
    users who want to consume the stdout output into a file (or pipe it)
    doesn't also get the log data.
    
    Example:
    ```
    \# Piping stdout to /dev/null to hide output
    $ java -jar cli/target/bacon.jar pnc product list > /dev/null
    
    \# Verbose flag used
    $ java -jar cli/target/bacon.jar pnc product list --verbose > /dev/null
    [DEBUG] - Log level set to DEBUG
    [INFO ] - Bootstrapping http engine with request retry handler...
    [DEBUG] - Requesting: GET http://<url>/pnc-rest-new/rest-new/generic-setting/announcement-banner Headers: Accept:[application/json].
    [INFO ] - Bootstrapping http engine with request retry handler...
    [DEBUG] - Loading first page.
    [DEBUG] - Requesting: GET http://<url>/pnc-rest-new/rest-new/products?pageSize=50&pageIndex=0 Headers: Accept:[application/json].
    ```

--- common/src/main/java/org/jboss/pnc/bacon/common/ObjectHelper.java
+++ common/src/main/java/org/jboss/pnc/bacon/common/ObjectHelper.java
@@ -1,10 +1,23 @@
 package org.jboss.pnc.bacon.common;
 
+import ch.qos.logback.classic.Level;
+
 public class ObjectHelper {
 
     public static void executeIfNotNull(Object value, Runnable run) {
         if (value != null) {
             run.run();
         }
+    }
+
+    public static void setRootLoggingLevel(Level level) {
+        ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory
+                .getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+        root.setLevel(level);
+    }
+
+    public static void setLoggingLevel(String loggerName, Level level) {
+        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(loggerName);
+        logger.setLevel(level);
     }
 }
