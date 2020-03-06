d4f69880aac800dd39e01a238bf4eb8caab75e4f
Dustin Kut Moy Cheung <dcheung@redhat.com>
2020-03-05 20:43:10 -0500

    Minor refactorings to bacon_install.py

--- bacon_install.py
+++ bacon_install.py
@@ -38,14 +38,14 @@ import urllib.request as request
 import xml.etree.ElementTree as ET
 
 
-MAVEN_CENTRAL_LINK = "https://repo.maven.org/maven2/org/jboss/pnc/ear-package/"
+MAVEN_CENTRAL_LINK = "https://repo.maven.org/maven2/org/jboss/pnc/bacon/cli/"
 MAVEN_SNAPSHOT_LINK = "https://repository.jboss.org/nexus/content/repositories/snapshots/org/jboss/pnc/bacon/cli/"
 
 USER_BACON_JAR_FOLDER_LOCATION = os.getenv("HOME") + "/.pnc-bacon/bin"
 ROOT_BACON_JAR_FOLDER_LOCATION = "/opt/bacon/bin"
 
-USER_SHELL_LOCATION = os.getenv("HOME") + "/bin"
-ROOT_SHELL_LOCATION = "/usr/local/bin"
+USER_SHELL_FOLDER_LOCATION = os.getenv("HOME") + "/bin"
+ROOT_SHELL_FOLDER_LOCATION = "/usr/local/bin"
 
 TEMPLATE_BASH = """
 #!/bin/bash
@@ -81,11 +81,44 @@ if [ -z "$1" ]; then
 fi
 """
 
+
+def download_maven_metadata_xml(url, folder):
+    """
+    Download the maven-metadata.xml for bacon and put it in folder
+
+    url must be without maven-metadata.xml
+    """
+    if not url.endswith("/"):
+        url = url + "/"
+
+    link = url + "maven-metadata.xml"
+    download_link(link, folder, "maven-metadata.xml")
+
+
+def download_link(link, folder, filename):
+    """
+    Download the link into the folder with name 'filename'
+    """
+
+    print("Downloading: " + link)
+    try:
+        r = request.urlopen(link).read()
+
+        with open(folder + "/" + filename, "wb") as f:
+            f.write(r)
+    except Exception:
+        raise Exception("Something wrong happened while downloading the link: " + link)
+
+
+def create_folder_if_absent(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
 class BaconInstall:
     """
     Object responsible with installing bacon
     """
-    def __init__(self, bacon_jar_location, shell_location, maven_url, snapshot = False):
+    def __init__(self, bacon_jar_location, shell_location, maven_url, snapshot=False):
 
         self.bacon_jar_location = bacon_jar_location
         self.shell_location = shell_location
@@ -101,8 +134,8 @@ class BaconInstall:
         """
         latest_version = self.__get_latest_version()
 
-        self.__create_folder_if_absent(self.bacon_jar_location)
-        self.__create_folder_if_absent(self.shell_location)
+        create_folder_if_absent(self.bacon_jar_location)
+        create_folder_if_absent(self.shell_location)
 
         self.__download_latest_version()
         self.__create_bacon_shell_script()
@@ -118,16 +151,13 @@ class BaconInstall:
 
         if self.snapshot:
             snapshot_version = self.__get_latest_snapshot_version()
-            download_link = self.maven_url + \
+            url = self.maven_url + \
                 latest_version + "/cli-" + snapshot_version + ".jar"
         else:
-            download_link = self.maven_url + \
+            url = self.maven_url + \
                 latest_version + "/cli-" + latest_version + ".jar"
 
-        self.__download_link(
-            download_link,
-            self.bacon_jar_location,
-            "bacon.jar")
+        download_link(url, self.bacon_jar_location, "bacon.jar")
 
         print("bacon installed in: {}".format(
             self.bacon_jar_location + "/bacon.jar"))
@@ -140,7 +170,7 @@ class BaconInstall:
 
         if self.latest_version is None:
             temp_folder = tempfile.mkdtemp()
-            self.__download_maven_metadata_xml(self.maven_url, temp_folder)
+            download_maven_metadata_xml(self.maven_url, temp_folder)
             root = ET.parse(temp_folder + "/maven-metadata.xml").getroot()
 
             if self.snapshot:
@@ -158,39 +188,12 @@ class BaconInstall:
 
         url = self.maven_url + latest_version
         temp_folder = tempfile.mkdtemp()
-        self.__download_maven_metadata_xml(url, temp_folder)
+        download_maven_metadata_xml(url, temp_folder)
 
         root = ET.parse(temp_folder + "/maven-metadata.xml").getroot()
 
         latest_tag = root.findall("versioning/snapshotVersions/snapshotVersion")
         return latest_tag[0].find("value").text
-
-    def __download_maven_metadata_xml(self, url, folder):
-        """
-        Download the maven-metadata.xml for bacon and put it in folder
-
-        url must be without maven-metadata.xml
-        """
-        if not url.endswith("/"):
-            url = url + "/"
-
-        download_link = url + "maven-metadata.xml"
-        self.__download_link(download_link, folder, "maven-metadata.xml")
-
-    def __download_link(self, link, folder, filename):
-
-        print("Downloading: " + link)
-        try:
-            r = request.urlopen(link).read()
-
-            with open(folder + "/" + filename, "wb") as f:
-                f.write(r)
-        except Exception:
-            raise Exception("Something wrong happened while downloading the link: " + link)
-
-    def __create_folder_if_absent(self, folder):
-        if not os.path.exists(folder):
-            os.makedirs(folder)
 
     def __create_bacon_shell_script(self):
 
@@ -218,11 +221,11 @@ def main():
         snapshot = False
 
     bacon_jar_location = USER_BACON_JAR_FOLDER_LOCATION
-    shell_location = USER_SHELL_LOCATION
+    shell_location = USER_SHELL_FOLDER_LOCATION
 
     if is_root():
         bacon_jar_location = ROOT_BACON_JAR_FOLDER_LOCATION
-        shell_location = ROOT_SHELL_LOCATION
+        shell_location = ROOT_SHELL_FOLDER_LOCATION
 
     bacon_install = BaconInstall(
             bacon_jar_location,
