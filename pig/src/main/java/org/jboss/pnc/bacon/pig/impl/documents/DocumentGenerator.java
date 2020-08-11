0590814da6ed02e471e4b3fc2bcba36579609677
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2020-08-11 14:03:55 +0200

    releaseStorageUrl moved to arguments, continuing pig builds by default, overriding pig context location with PIG_CONTEXT_LOCATION env variable

--- pig/src/main/java/org/jboss/pnc/bacon/pig/impl/documents/DocumentGenerator.java
+++ pig/src/main/java/org/jboss/pnc/bacon/pig/impl/documents/DocumentGenerator.java
@@ -17,6 +17,15 @@
  */
 package org.jboss.pnc.bacon.pig.impl.documents;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.jboss.pnc.bacon.config.Config;
 import org.jboss.pnc.bacon.pig.impl.config.PigConfiguration;
 import org.jboss.pnc.bacon.pig.impl.documents.sharedcontent.SharedContentReportGenerator;
@@ -26,15 +35,6 @@ import org.jboss.pnc.bacon.pig.impl.repo.RepositoryData;
 import org.jboss.pnc.bacon.pig.impl.utils.GAV;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author Michal Szynkiewicz, michal.l.szynkiewicz@gmail.com <br>
@@ -60,9 +60,9 @@ public class DocumentGenerator {
     }
 
     public void generateDocuments(Map<String, PncBuild> builds, RepositoryData repo) {
-        String repositoryContents = repo.getGavs().stream().map(GAV::toGav).collect(Collectors.joining("\n"));
-        String duplicates = repo.getGavs()
-                .stream()
+        Collection<GAV> gavs = repo.getGavs();
+        String repositoryContents = gavs.stream().map(GAV::toGav).collect(Collectors.joining("\n"));
+        String duplicates = gavs.stream()
                 .collect(Collectors.groupingBy(GAV::getGa))
                 .values()
                 .stream()
