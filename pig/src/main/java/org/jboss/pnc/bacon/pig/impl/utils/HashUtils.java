30d21482b339d392536876209f78eb0db00860de
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2020-08-28 17:15:29 +0200

    start clean when config modified

---
+++ pig/src/main/java/org/jboss/pnc/bacon/pig/impl/utils/HashUtils.java
@@ -0,0 +1,32 @@
+package org.jboss.pnc.bacon.pig.impl.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.jboss.resteasy.util.Hex;
+
+public class HashUtils {
+    private HashUtils() {
+    }
+
+    public static String hashDirectory(Path directory) {
+        MessageDigest sha = DigestUtils.getSha512Digest();
+        try {
+            Files.walk(directory).filter(Files::isRegularFile).sorted().forEach(path -> {
+                try {
+                    DigestUtils.updateDigest(sha, directory.relativize(path).toString());
+                    DigestUtils.updateDigest(sha, path.toFile());
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to calculate sha of " + path.toAbsolutePath(), e);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to walk through " + directory, e);
+        }
+        return Hex.encodeHex(sha.digest());
+    }
+
+}
---
+++ pig/src/test/java/org/jboss/pnc/bacon/pig/impl/utils/HashUtilsTest.java
@@ -0,0 +1,62 @@
+package org.jboss.pnc.bacon.pig.impl.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+class HashUtilsTest {
+    static final Path testDirs = Paths.get("src", "test", "resources", "hash");
+
+    @Test
+    void shouldGiveTheSameHashForUnmodified() {
+        String originalHash = HashUtils.hashDirectory(testDirs.resolve("original"));
+        String exactCopyHash = HashUtils.hashDirectory(testDirs.resolve("copy"));
+        assertThat(originalHash).isEqualTo(exactCopyHash);
+    }
+
+    @Test
+    void shouldGiveTheSameHashTwice() {
+        String originalHash = HashUtils.hashDirectory(testDirs.resolve("original"));
+        String secondRunHash = HashUtils.hashDirectory(testDirs.resolve("original"));
+        assertThat(originalHash).isEqualTo(secondRunHash);
+    }
+
+    @Test
+    void shouldGiveDifferentHashForAdditionalNewline() {
+        String originalHash = HashUtils.hashDirectory(testDirs.resolve("original"));
+        String exactCopyHash = HashUtils.hashDirectory(testDirs.resolve("additional-newline"));
+        assertThat(originalHash).isNotEqualTo(exactCopyHash);
+    }
+
+    @Test
+    void shouldGiveDifferentHashForMovedFile() {
+        String originalHash = HashUtils.hashDirectory(testDirs.resolve("original"));
+        String exactCopyHash = HashUtils.hashDirectory(testDirs.resolve("moved-file"));
+        assertThat(originalHash).isNotEqualTo(exactCopyHash);
+    }
+
+    @Test
+    void shouldGiveDifferentHashForRenamedFile() {
+        String originalHash = HashUtils.hashDirectory(testDirs.resolve("original"));
+        String exactCopyHash = HashUtils.hashDirectory(testDirs.resolve("renamed-file"));
+        assertThat(originalHash).isNotEqualTo(exactCopyHash);
+    }
+
+    @Test
+    void shouldGiveDifferentHashForRemovedFile() {
+        String originalHash = HashUtils.hashDirectory(testDirs.resolve("original"));
+        String exactCopyHash = HashUtils.hashDirectory(testDirs.resolve("removed-file"));
+        assertThat(originalHash).isNotEqualTo(exactCopyHash);
+    }
+
+    @Test
+    void shouldGiveDifferentHashForAddedFile() {
+        String originalHash = HashUtils.hashDirectory(testDirs.resolve("original"));
+        String exactCopyHash = HashUtils.hashDirectory(testDirs.resolve("additional-file"));
+        assertThat(originalHash).isNotEqualTo(exactCopyHash);
+    }
+
+}
