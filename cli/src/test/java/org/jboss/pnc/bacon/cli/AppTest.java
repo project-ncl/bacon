7a2e403aee494ee3f8b654f47b951d621f8e17f0
Nick Cross <ncross@redhat.com>
2020-09-08 10:12:23 +0100

    NCL5963 picocli : fix config path and add some tests

--- cli/src/test/java/org/jboss/pnc/bacon/cli/AppTest.java
+++ cli/src/test/java/org/jboss/pnc/bacon/cli/AppTest.java
@@ -2,6 +2,9 @@ package org.jboss.pnc.bacon.cli;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErr;
 import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -24,5 +27,58 @@ class AppTest {
                         0,
                         app.run(new String[] { "-v", "pnc", "admin", "maintenance-mode", "activate", "-h" })));
         assertTrue(text.contains("bacon pnc admin maintenance-mode activate \"Switching"));
+    }
+
+    @Test
+    void testConfigPath() throws Exception {
+        String text = tapSystemErr(
+                () -> assertEquals(
+                        0,
+                        new App().run(new String[] { "-p", "/tmp/123456789", "-v", "pnc", "-o", "build", "-h" })));
+        assertTrue(text.contains("Config file set from flag to /tmp/123456789/config.yaml"));
+    }
+
+    @Test
+    void testJSONEnabled1() throws Exception {
+        File pncClasses = new File(App.class.getClassLoader().getResource("").getFile());
+        File configYaml = new File(
+                pncClasses.getParentFile().getParentFile().getParentFile(),
+                "pnc" + File.separator + "target" + File.separator + "test-classes");
+
+        String text = tapSystemErr(
+                () -> assertEquals(
+                        1,
+                        new App().run(
+                                new String[] { "-p", configYaml.toString(), "-v", "pnc", "-o", "build", "get", "0" })));
+        assertTrue(text.contains("JSON command is enabled: true"));
+    }
+
+    @Test
+    void testJSONEnabled2() throws Exception {
+        File pncClasses = new File(App.class.getClassLoader().getResource("").getFile());
+        File configYaml = new File(
+                pncClasses.getParentFile().getParentFile().getParentFile(),
+                "pnc" + File.separator + "target" + File.separator + "test-classes");
+
+        String text = tapSystemErr(
+                () -> assertEquals(
+                        1,
+                        new App().run(
+                                new String[] { "-o", "-p", configYaml.toString(), "-v", "pnc", "build", "get", "0" })));
+        assertTrue(text.contains("JSON command is enabled: true"));
+    }
+
+    @Test
+    void testJSONDisabled() throws Exception {
+        File pncClasses = new File(App.class.getClassLoader().getResource("").getFile());
+        File configYaml = new File(
+                pncClasses.getParentFile().getParentFile().getParentFile(),
+                "pnc" + File.separator + "target" + File.separator + "test-classes");
+
+        String text = tapSystemErr(
+                () -> assertEquals(
+                        1,
+                        new App().run(new String[] { "-p", configYaml.toString(), "-v", "pnc", "build", "get", "0" })));
+        assertTrue(text.contains("JSON command is enabled: false"));
     }
 }
