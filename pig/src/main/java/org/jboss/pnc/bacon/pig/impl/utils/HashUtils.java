62c89c207ee37abb52b6d2125dcea9a4f26841b7
Dustin Kut Moy Cheung <dcheung@redhat.com>
2020-10-30 15:44:26 -0400

    [NCLSUP-174] Ignore pig-context and target folder
    When generating the hash of the directory config to determine whether
    the pig-context.json should be invalidated or not, the hash should
    ignore the files 'bacon/pig-context.json' and the contents inside the
    'target' folder.
    
    This is to handle a special case where the user may run the `bacon pig`
    command using path '.', in which case the 2 files mentioned above will
    be created inside the directory config.
    
    Since those 2 files change content all the time, it causes the hash of
    the directory to be always different between `bacon pig` runs and
    wrongly invalidates the existing pig-context.json because it wrongly
    assumes that the *real* content of the directory config has changed.

--- pig/src/main/java/org/jboss/pnc/bacon/pig/impl/utils/HashUtils.java
+++ pig/src/main/java/org/jboss/pnc/bacon/pig/impl/utils/HashUtils.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class HashUtils {
@@ -14,9 +15,22 @@ public class HashUtils {
     }
 
     public static String hashDirectory(Path directory) {
+        // don't ignore any file
+        return hashDirectory(directory, path -> false);
+    }
+
+    /**
+     * Hash the content of a directory and specify files to ignore in the hash
+     *
+     * @param directory path of the directory
+     * @param ignorePredicate predicate that get the list of files, and decides whether to ignore them or not. returns
+     *        true to ignore
+     * @return the hash of the directory content
+     */
+    public static String hashDirectory(Path directory, Predicate<Path> ignorePredicate) {
         MessageDigest sha = DigestUtils.getSha512Digest();
         try (Stream<Path> stream = Files.walk(directory)) {
-            stream.filter(Files::isRegularFile).sorted().forEach(path -> {
+            stream.filter(Files::isRegularFile).filter(ignorePredicate.negate()).sorted().forEach(path -> {
                 try {
                     DigestUtils.updateDigest(sha, directory.relativize(path).toString());
                     DigestUtils.updateDigest(sha, path.toFile());
