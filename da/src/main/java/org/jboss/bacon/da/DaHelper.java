726d0b2ae219bcf7e735791204ea05e962f837da
Dustin Kut Moy Cheung <dcheung@redhat.com>
2022-02-16 15:08:19 -0500

    Add blocklist support for DA

--- da/src/main/java/org/jboss/bacon/da/DaHelper.java
+++ da/src/main/java/org/jboss/bacon/da/DaHelper.java
@@ -20,15 +20,24 @@ package org.jboss.bacon.da;
 import org.jboss.bacon.da.rest.endpoint.ListingsApi;
 import org.jboss.bacon.da.rest.endpoint.LookupApi;
 import org.jboss.bacon.da.rest.endpoint.ReportsApi;
+import org.jboss.da.listings.model.rest.RestArtifact;
 import org.jboss.da.model.rest.GAV;
 import org.jboss.da.model.rest.NPMPackage;
 import org.jboss.pnc.bacon.common.Utils;
 import org.jboss.pnc.bacon.config.Config;
 import org.jboss.pnc.bacon.config.DaConfig;
+import org.jboss.pnc.bacon.pnc.client.PncClientHelper;
+import org.jboss.pnc.client.Configuration;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.MultivaluedMap;
+
+import java.io.IOException;
 
 /**
  * Helper methods for DA stuff
@@ -55,12 +64,24 @@ public class DaHelper {
         return builder.build().target(daUrl);
     }
 
+    private static ResteasyWebTarget getAuthenticatedClient() {
+
+        ResteasyWebTarget target = getClient();
+        Configuration pncConfiguration = PncClientHelper.getPncConfiguration();
+        target.register(new TokenAuthenticator(pncConfiguration.getBearerToken()));
+        return target;
+    }
+
     public static ReportsApi createReportsApi() {
         return getClient().proxy(ReportsApi.class);
     }
 
     public static ListingsApi createListingsApi() {
         return getClient().proxy(ListingsApi.class);
+    }
+
+    public static ListingsApi createAuthenticatedListingsApi() {
+        return getAuthenticatedClient().proxy(ListingsApi.class);
     }
 
     public static LookupApi createLookupApi() {
@@ -110,10 +131,32 @@ public class DaHelper {
     }
 
     /**
+     * Transforms a string in format group:artifact:version to a RestArtifact If the string is not properly formatted, a
+     * RuntimeException is thrown
+     *
+     * @param gav String to transform
+     *
+     * @return RestArtifact object
+     */
+    public static RestArtifact toRestArtifact(String gav) {
+        String[] pieces = gav.split(":");
+
+        if (pieces.length != 3) {
+            throw new RuntimeException("GAV " + gav + " cannot be parsed into groupid:artifactid:version");
+        }
+        RestArtifact artifact = new RestArtifact();
+        artifact.setGroupId(pieces[0]);
+        artifact.setArtifactId(pieces[1]);
+        artifact.setVersion(pieces[2]);
+
+        return artifact;
+    }
+
+    /**
      * Transforms a string in format package:version to an NPMPackage If the string is not properly formatted, a
      * RuntimeException is thrown
      *
-     * @param npmVersion String to transform
+     * @param nameVersion String to transform
      *
      * @return NPMPackage object
      */
@@ -125,5 +168,19 @@ public class DaHelper {
         }
 
         return new NPMPackage(pieces[0], pieces[1]);
+    }
+
+    private static class TokenAuthenticator implements ClientRequestFilter {
+
+        private final String token;
+
+        TokenAuthenticator(String token) {
+            this.token = token;
+        }
+
+        public void filter(ClientRequestContext requestContext) throws IOException {
+            MultivaluedMap<String, Object> headers = requestContext.getHeaders();
+            headers.add("Authorization", "Bearer " + this.token);
+        }
     }
 }
