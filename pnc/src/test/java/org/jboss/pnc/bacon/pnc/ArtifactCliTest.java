690979620d1d5383c8588926fc9a34475aa0a573
Dustin Kut Moy Cheung <dcheung@redhat.com>
2022-07-20 16:22:10 -0400

    [NCL-7241] Supporting GAV instead of GATV to find PNC build config via bacon

---
+++ pnc/src/test/java/org/jboss/pnc/bacon/pnc/ArtifactCliTest.java
@@ -0,0 +1,22 @@
+package org.jboss.pnc.bacon.pnc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtifactCliTest {
+
+    @Test
+    void testTransformIdentifierIfGAV() {
+        String gav1 = "xom:xom:1.2.5";
+        String identifier1 = "xom:xom:pom:1.2.5";
+
+        String identifier2 = "toe:thumb:jar:2.0.0";
+
+        String identifier3 = "hello:world:pom:1.2.3:test";
+
+        assertEquals(ArtifactCli.transformIdentifierIfGAV(gav1), identifier1);
+        assertEquals(ArtifactCli.transformIdentifierIfGAV(identifier2), identifier2);
+        assertEquals(ArtifactCli.transformIdentifierIfGAV(identifier3), identifier3);
+    }
+}
