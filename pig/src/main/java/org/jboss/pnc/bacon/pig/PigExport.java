069b92f38c40011185be29c45bf873a83ab71e36
Honza Brázdil <jbrazdil@redhat.com>
2022-10-27 17:22:16 +0200

    Add new options to pig export build-config
    --revision to select build config revision to export
    --environment-by-name to export with environmentName instead of
    environmentImageID
    --rename to set the name of the exported build config

--- pig/src/main/java/org/jboss/pnc/bacon/pig/PigExport.java
+++ pig/src/main/java/org/jboss/pnc/bacon/pig/PigExport.java
@@ -7,7 +7,10 @@ import org.jboss.pnc.bacon.pnc.common.ClientCreator;
 import org.jboss.pnc.client.BuildConfigurationClient;
 import org.jboss.pnc.client.ClientException;
 import org.jboss.pnc.dto.BuildConfiguration;
+import org.jboss.pnc.dto.BuildConfigurationRevision;
 import picocli.CommandLine;
+
+import java.util.Optional;
 
 @CommandLine.Command(
         name = "export",
@@ -20,6 +23,21 @@ public class PigExport {
             description = "Export a build-config in a format that can be injected into PiG's build-config.yaml")
     public static class BuildConfigExport extends AbstractGetSpecificCommand<BuildConfig> {
 
+        @CommandLine.Option(
+                names = "--revision",
+                description = "Revision of the build config. By default the current revision is used.")
+        private Integer revision;
+
+        @CommandLine.Option(
+                names = "--environment-by-name",
+                description = "Export config with environment name instead of environment image id.")
+        private boolean envByName = false;
+
+        @CommandLine.Option(
+                names = "--rename",
+                description = "Use this name for the exported build config instead of the original one.")
+        private String rename;
+
         private static final ClientCreator<BuildConfigurationClient> CREATOR = new ClientCreator<>(
                 BuildConfigurationClient::new);
 
@@ -27,7 +45,16 @@ public class PigExport {
         public BuildConfig getSpecific(String id) throws ClientException {
             try (BuildConfigurationClient client = CREATOR.newClient()) {
                 BuildConfiguration bc = client.getSpecific(id);
-                return BuildConfigMapping.toBuildConfig(bc);
+                BuildConfigMapping.GeneratorOptions opts = BuildConfigMapping.GeneratorOptions.builder()
+                        .useEnvironmentName(envByName)
+                        .nameOverride(Optional.ofNullable(rename))
+                        .build();
+                if (revision != null) {
+                    BuildConfigurationRevision bcr = client.getRevision(id, revision);
+                    return BuildConfigMapping.toBuildConfig(bc, bcr, opts);
+                } else {
+                    return BuildConfigMapping.toBuildConfig(bc, opts);
+                }
             }
         }
     }
