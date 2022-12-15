482c2c6b210f8707df5b7ff9e37692c8d758d0d9
Honza Brázdil <jbrazdil@redhat.com>
2022-12-15 15:02:16 +0100

    Change type for buildConfig in FoundProject
    ~ Fix typo in FoundProjects

--- temp-name/src/main/java/org/jboss/bacon/tempname/impl/projectfinder/FoundProject.java
+++ temp-name/src/main/java/org/jboss/bacon/tempname/impl/projectfinder/FoundProject.java
@@ -2,13 +2,14 @@ package org.jboss.bacon.tempname.impl.projectfinder;
 
 import lombok.Data;
 import org.jboss.da.model.rest.GAV;
-import org.jboss.pnc.dto.BuildConfiguration;
+import org.jboss.pnc.dto.BuildConfigurationRevision;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Data
 public class FoundProject {
     private Set<GAV> gavs; // maybe use different GAV class?
-    private BuildConfiguration buildConfig;
+    private Optional<BuildConfigurationRevision> buildConfig;
     private boolean exactMatch;
 }
