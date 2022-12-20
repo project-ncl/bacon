11f989ba8b22d1872100f286ab399f3f2a10666d
Honza Brázdil <jbrazdil@redhat.com>
2022-12-20 21:58:08 +0100

    Implement Project Finder in Autobuilder

--- temp-name/src/main/java/org/jboss/bacon/tempname/impl/projectfinder/FoundProjects.java
+++ temp-name/src/main/java/org/jboss/bacon/tempname/impl/projectfinder/FoundProjects.java
@@ -2,9 +2,10 @@ package org.jboss.bacon.tempname.impl.projectfinder;
 
 import lombok.Data;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
 public class FoundProjects {
-    Set<FoundProject> foundProjects;
+    private Set<FoundProject> foundProjects = new HashSet<>();
 }
