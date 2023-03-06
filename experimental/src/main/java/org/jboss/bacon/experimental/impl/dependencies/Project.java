9a5c4411e82553dbe48f198b45207a8a0e0fa72c
Honza Brázdil <jbrazdil@redhat.com>
2023-03-06 17:43:14 +0100

    [AB] Order build configs and dependencies in the output

--- experimental/src/main/java/org/jboss/bacon/experimental/impl/dependencies/Project.java
+++ experimental/src/main/java/org/jboss/bacon/experimental/impl/dependencies/Project.java
@@ -8,13 +8,14 @@ import org.jboss.da.model.rest.GAV;
 import java.util.Set;
 
 @Data
-@ToString(exclude = "dependencies")
-@EqualsAndHashCode(exclude = "dependencies")
+@ToString(exclude = { "dependencies", "depth" })
+@EqualsAndHashCode(exclude = { "dependencies", "depth" })
 public class Project {
     private String sourceCodeURL;
     private String sourceCodeRevision;
     private Set<GAV> gavs;
     private Set<Project> dependencies;
+    private int depth = -1;
     private boolean cutDepenendecy = false;
 
     public GAV getFirstGAV() {
