f9173affcd9f2706086810778c89aa6a9939ca60
Harsh Madhani <harshmadhani@gmail.com>
2024-04-30 14:35:23 +0530

    feat: implement a global excludes to exclude specific builds from autobuilder

--- experimental/src/main/java/org/jboss/bacon/experimental/impl/dependencies/DependencyResult.java
+++ experimental/src/main/java/org/jboss/bacon/experimental/impl/dependencies/DependencyResult.java
@@ -2,8 +2,10 @@ package org.jboss.bacon.experimental.impl.dependencies;
 
 import lombok.Data;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Predicate;
 
 @Data
 public class DependencyResult {
@@ -21,4 +23,15 @@ public class DependencyResult {
             project.getDependencies().forEach(p -> fillAllProject(allProjects, p));
         }
     }
+
+    /**
+     * Remove the provided GAV from the {@link #topLevelProjects} set.
+     *
+     * @param excludedGavs GAV to be excluded from the topLevelProjects.
+     */
+    public void removeProjects(String[] excludedGavs) {
+        Predicate<Project> pr = a -> (Arrays.stream(excludedGavs).anyMatch(a.getName()::equals));
+        topLevelProjects.removeIf(pr);
+    }
+
 }
---
+++ experimental/src/test/java/org/jboss/bacon/experimental/impl/dependencies/DependencyExcluderTest.java
@@ -0,0 +1,124 @@
+package org.jboss.bacon.experimental.impl.dependencies;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.jboss.pnc.bacon.config.Config;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.io.*;
+import java.net.URL;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DependencyExcluderTest {
+
+    @RegisterExtension
+    static WireMockExtension wireMock = WireMockExtension.newInstance()
+            .options(
+                    wireMockConfig().port(45656)
+                            .usingFilesUnderDirectory("src/test/resources/wiremock")
+                            .notifier(new ConsoleNotifier(false)))
+            .build();
+
+    private static WireMockServer mockServer;
+
+    @BeforeAll
+    static void setup() throws IOException {
+        File file = new File(DependencyExcluderTest.class.getClassLoader().getResource("config/config.yaml").getFile());
+        Config.configure(file.getParent(), "config.yaml", "default");
+        Config.initialize();
+    }
+
+    @Test
+    void parseEmpty() {
+        // DependencyExcluder dependencyExcluder = new DependencyExcluder();
+        String[] excludedDependencies = DependencyExcluder.getExcludedGavs("");
+        assertThat(excludedDependencies).isNotNull();
+        assertThat(excludedDependencies.length).isEqualTo(0);
+    }
+
+    @Test
+    void parseNull() {
+        // DependencyExcluder dependencyExcluder = new DependencyExcluder();
+        String[] excludedDependencies = DependencyExcluder.getExcludedGavs(null);
+        assertThat(excludedDependencies).isNotNull();
+        assertThat(excludedDependencies.length).isEqualTo(0);
+    }
+
+    @Test
+    void parseEmptyFile() {
+        try {
+            final String emptyExcludedFile = getFileString("excluded-empty.txt");
+            String[] excludedDependencies = DependencyExcluder.getExcludedGavs(emptyExcludedFile);
+        } catch (IOException e) {
+            assertThat(false).isTrue();
+        }
+    }
+
+    @Test
+    void parseSampleFile() {
+        try {
+            final String emptyExcludedFile = getFileString("excluded-sample.txt");
+            String[] excludedDependencies = DependencyExcluder.getExcludedGavs(emptyExcludedFile);
+            assertThat(excludedDependencies).isNotNull();
+            assertThat(excludedDependencies.length).isEqualTo(11);
+            assertThat(excludedDependencies[0]).isEqualTo("io.vertx:vertx-amqp-client:4.3.7");
+            assertThat(excludedDependencies[10]).isEqualTo("io.vertx:vertx-codegen:4.3.7");
+        } catch (IOException e) {
+            assertThat(false).isTrue();
+        }
+    }
+
+    @Test
+    void parseSampleFileFromHost() {
+        DependencyExcluder de = new DependencyExcluder(Config.instance().getActiveProfile().getDa());
+        String exclusionFileContents = de.fetchExclusionFile();
+        String[] excludedDependencies = DependencyExcluder.getExcludedGavs(exclusionFileContents);
+        assertThat(excludedDependencies).isNotNull();
+        assertThat(excludedDependencies.length).isEqualTo(10);
+        assertThat(excludedDependencies[0]).isEqualTo("io.vertx:vertx-amqp-client:4.3.7");
+        assertThat(excludedDependencies[4]).isEqualTo("junit:junit:4.13.2");
+        assertThat(excludedDependencies[9]).isEqualTo("io.vertx:vertx-codegen:4.3.7");
+    }
+
+    @Test
+    void missingGavExclusionFromBaconConfigShouldntFail() {
+        File file = new File(
+                DependencyExcluderTest.class.getClassLoader()
+                        .getResource("config/config-missing-gavExclusionUrl.yaml")
+                        .getFile());
+        Config.configure(file.getParent(), "config-missing-gavExclusionUrl.yaml", "default");
+        try {
+            Config.initialize();
+            DependencyExcluder de = new DependencyExcluder(Config.instance().getActiveProfile().getDa());
+            String exclusionFileContents = de.fetchExclusionFile();
+            String[] excludedDependencies = DependencyExcluder.getExcludedGavs(exclusionFileContents);
+            assertThat(excludedDependencies).isNotNull();
+            assertThat(excludedDependencies.length).isEqualTo(0);
+        } catch (IOException e) {
+            assertThat(false).isTrue();
+        }
+    }
+
+    public static String getFileString(final String fileName) throws IOException {
+        String fileContents = null;
+        ClassLoader classLoader = DependencyExcluderTest.class.getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        InputStream resourceStream = classLoader.getResourceAsStream(fileName);
+        if (resource != null) {
+            StringBuilder textBuilder = new StringBuilder();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(resourceStream))) {
+                int c;
+                while ((c = br.read()) != -1) {
+                    textBuilder.append((char) c);
+                }
+            }
+            fileContents = textBuilder.toString();
+        }
+        return fileContents;
+    }
+}
