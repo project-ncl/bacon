844bd31e4bbff68347e89d1bd7be7aa37fc05fef
Honza Brázdil <jbrazdil@redhat.com>
2024-05-02 12:12:51 +0200

    [AB] Add javadoc to autobuilder config

--- experimental/src/main/java/org/jboss/bacon/experimental/impl/config/DefaultBuildConfigValues.java
+++ experimental/src/main/java/org/jboss/bacon/experimental/impl/config/DefaultBuildConfigValues.java
@@ -7,15 +7,35 @@ import org.jboss.pnc.dto.validation.constraints.SCMUrl;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+/**
+ * Default values to be used when generating build config.
+ */
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class DefaultBuildConfigValues {
+    /**
+     * Name of the environment to be used when generating new build config. Required.
+     */
     @NotNull
     private String environmentName;
+
+    /**
+     * Build script to be used when generating new build config.
+     */
     @NotEmpty
     private String buildScript = "mvn deploy";
+
+    /**
+     * Placeholder SCM URL.
+     *
+     * To be used when no SCM url was found for the project. We need it to generate valid build-config.
+     */
     @SCMUrl(message = "must be valid git url")
     private String scmUrl = "https://github.com/michalszynkiewicz/empty.git";
+
+    /**
+     * Placeholder SCM revision to be used when using placeholder URL or when SCM revision was not resolved.
+     */
     @NotEmpty
     private String scmRevision = "master";
 }
