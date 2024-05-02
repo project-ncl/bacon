8414e63b7164d72b7377be7664294dce861f92db
Honza Brázdil <jbrazdil@redhat.com>
2024-05-02 12:12:51 +0200

    [AB] Add javadoc to autobuilder config

--- experimental/src/main/java/org/jboss/bacon/experimental/impl/config/BuildConfigGeneratorConfig.java
+++ experimental/src/main/java/org/jboss/bacon/experimental/impl/config/BuildConfigGeneratorConfig.java
@@ -10,21 +10,59 @@ import javax.validation.constraints.NotNull;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
+/**
+ * Configuration for how the new build configs should be created.
+ */
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class BuildConfigGeneratorConfig {
+    /**
+     * Template of the PiG build-config.yaml to use.
+     */
     @NotNull
     private PigConfiguration pigTemplate;
+
+    /**
+     * SCM URLs which contain one of the string listed will be replaced by the default placeholder scmUrl.
+     */
     @NotNull
     private Set<String> scmReplaceWithPlaceholder = Set.of();
+
+    /**
+     * SCM URLs will have the listed key substring replaced with the value.
+     */
     @NotNull
     private LinkedHashMap<String, String> scmPattern = new LinkedHashMap<>();
+
+    /**
+     * SCM URLs containing the listed key are wholly replaced by the value.
+     */
     @NotNull
     private LinkedHashMap<String, String> scmMapping = new LinkedHashMap<>();
+
+    /**
+     * When generating new or copying and changing existing build config, will add 'false' at the end of the build
+     * script to make sure the build fails.
+     *
+     * This can be helpful to make sure the configs are manually reviewed.
+     */
     private boolean failGeneratedBuildScript = true;
+
+    /**
+     * Default values to be used when generating build config
+     */
     @Valid
     @NotNull
     private DefaultBuildConfigValues defaultValues = new DefaultBuildConfigValues();
+
+    /**
+     * If true, uses systemImageId to specify environment (to force use of specific environment) when the environment
+     * defined by environmentName would not be found because it was deprecated.
+     */
     private boolean allowDeprecatedEnvironments = false;
+
+    /**
+     * If true, will reuse existing autobuilder managed configs without change.
+     */
     private boolean reuseAutobuilderConfigs = false;
 }
--- experimental/src/main/java/org/jboss/bacon/experimental/impl/config/DependencyResolutionConfig.java
+++ experimental/src/main/java/org/jboss/bacon/experimental/impl/config/DependencyResolutionConfig.java
@@ -8,17 +8,54 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Configuration for the autobuilder dependency resolution.
+ */
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class DependencyResolutionConfig {
-    @NotNull
-    private Set<String> excludeArtifacts = Set.of();
-    @NotNull
-    private Set<String> includeArtifacts = Set.of();
+
+    /**
+     * List of toplevel artifacts that should be analyzed and built.
+     */
     @NotNull
     private Set<String> analyzeArtifacts = Set.of();
+
+    /**
+     * Bom that should be analyzed and which artifacts should be built.
+     */
     private String analyzeBOM;
-    private List<String> recipeRepos = List.of();
+
+    /**
+     * Whether optional dependencies should be included in the analysis.
+     */
     private boolean includeOptionalDependencies = true;
+
+    /**
+     * List of artifacts that should be excluded from the analysis.
+     *
+     * The matching artifact and it's dependencies will be omitted from the resulting build tree. The format is
+     * G:A:[C:T:]V where every part can be replaced with '*'.
+     */
+    @NotNull
+    private Set<String> excludeArtifacts = Set.of();
+
+    /**
+     * If set to true, all -redhat-X artifacts and their dependencies are omitted from the resulting build tree.
+     */
     private boolean excludeProductizedArtifacts = false;
+
+    /**
+     * List of artifacts that should not be excluded from the analysis.
+     *
+     * The format is G:A:[C:T:]V where every part can be replaced with '*'.
+     */
+    @NotNull
+    private Set<String> includeArtifacts = Set.of();
+
+    /**
+     * The list of URLs to use for the SCM recipes. The SCM recipes are used for determining SCM coordinates for
+     * project.
+     */
+    private List<String> recipeRepos = List.of();
 }
